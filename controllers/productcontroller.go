33f2a80067fa6b5b2e63af1b997e017dfd57e1d2
Yigaue <lekiagospel@gmail.com>
2022-11-09 14:19:02 +0100

    getproducts, updateproducts,deleteproducts

--- controllers/productcontroller.go
+++ controllers/productcontroller.go
@@ -39,3 +39,38 @@ func GetProductById(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(product)
 }
+
+func GetProducts(w http.ResponseWriter, r *http.Request) {
+	var products []entities.Product
+	database.Instance.Find(&products)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(products)
+}
+
+func UpdateProduct(w http.ResponseWriter, r *http.Request) {
+	productId := mux.Vars(r)["id"]
+	if !checkIfProductExist(productId) {
+		json.NewEncoder(w).Encode("Product not found")
+		return
+	}
+	var product entities.Product
+	database.Instance.First(&product, productId)
+	json.NewDecoder(r.Body).Decode(&product)
+	database.Instance.Save(&product)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(product)
+}
+
+func DeleteProduct(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	productId := mux.Vars(r)["id"]
+	if !checkIfProductExist(productId) {
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode("Product not found")
+		return
+	}
+	var product entities.Product
+	database.Instance.Delete(&product, productId)
+	json.NewEncoder(w).Encode("Product deleted successfully")
+}
